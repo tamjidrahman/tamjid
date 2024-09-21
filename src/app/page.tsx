77a9a94e2fa5a180aa1cd7ed965babf4ae680e6d
Tamjid Rahman <tamjidrahman@Tamjids-MacBook-Pro.local>
2024-09-21 15:06:24 -0400

    clean up imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import { Mail, GitBranch, Sun, Moon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-
 import * as React from "react";
-import Link from "next/link";
 
 import { TopNavBar } from "@/components/TopNavBar";
 import { ModeToggle } from "@/components/mode-toggle";
