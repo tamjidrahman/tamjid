7e57e8e148e637be8a50e1d429475facf9d06db0
Tamjid Rahman <tamjidrahman@Tamjids-MacBook-Pro.local>
2024-09-22 14:47:49 -0400

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,27 @@ export default function Home() {
           content="I'm a Software Engineering Manager in Cambridge, MA"
           key="desc"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <div className="flex justify-center mt-20">
         <h1 className="text-4xl text-primary">👋, I&apos;m Tamjid</h1>
