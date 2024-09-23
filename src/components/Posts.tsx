8cac6e8f1166a3a069d430190ec9188b655da666
Tamjid Rahman <tamjidrahman@Tamjids-MacBook-Pro.local>
2024-09-22 20:12:26 -0400

    fix redgates id

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -9,22 +9,6 @@ export interface Post {
 
 export const posts: Post[] = [
   {
-    id: "1",
-    title: "Hello, world!",
-    description: "Testing",
-    date: new Date(),
-    tags: ["demo"],
-    body: `Lorem ipsum odor amet, consectetuer adipiscing elit. Aptent metus faucibus nibh convallis volutpat ad, feugiat penatibus. Ultrices in morbi felis risus sapien. Porttitor finibus iaculis aenean egestas ultricies ac potenti. Senectus litora torquent maecenas condimentum consectetur. Amet aliquet ad varius scelerisque mollis quisque; torquent etiam. Diam finibus porta consequat cras orci integer quis. Vivamus phasellus accumsan praesent metus sagittis mollis enim. Ullamcorper facilisi facilisis felis justo urna.
-
-Habitant at dictum mus aliquet dapibus elit. Faucibus maximus consequat quam hendrerit parturient himenaeos condimentum a. Imperdiet eu ornare eros penatibus sed conubia etiam egestas. Justo nec fermentum non augue eleifend primis mauris. Auctor aenean consectetur elementum justo adipiscing ante ultricies mus at. Magnis id consequat facilisis dictum interdum fermentum.
-
-Praesent euismod eu lacus; leo nam suscipit vehicula purus vitae. Penatibus sed ligula luctus laoreet nam malesuada ligula vel. Vulputate in fringilla parturient nostra primis facilisi cras? Malesuada malesuada hendrerit in ornare blandit. Vivamus turpis habitasse malesuada volutpat eros volutpat quam potenti? Quis ut ultrices; non vulputate curabitur consequat. Nostra ante nascetur facilisis magnis suscipit vitae.
-
-Consectetur dolor facilisi dis non tortor sociosqu lectus dignissim? Consectetur eu tellus adipiscing molestie nibh ridiculus mus massa. Et aliquam fringilla vulputate semper duis class. Praesent augue purus mus proin luctus. Fames luctus erat sociosqu maximus tellus. Laoreet arcu tempor malesuada gravida elit finibus condimentum. Ultrices ipsum mus pretium tempus semper fermentum vestibulum. Senectus pellentesque risus facilisis, donec morbi blandit. Turpis ad porttitor inceptos maecenas vulputate nascetur finibus vel. Aptent faucibus curae dictum massa tincidunt elementum faucibus gravida platea!
-
-Pretium morbi sodales blandit, eleifend aliquam aptent euismod nibh. Scelerisque congue dapibus senectus curabitur platea. Praesent ornare magna lorem; felis sem nec. Phasellus bibendum auctor per ullamcorper aenean iaculis. Nulla volutpat maximus platea justo felis ullamcorper. Malesuada maximus vivamus gravida feugiat montes interdum habitant tempus. Sem habitant at tortor sociosqu nibh rutrum commodo facilisis adipiscing. Ridiculus netus praesent nec senectus a convallis tortor lectus odio! Molestie ultrices natoque massa taciti sapien tempor metus.`,
-  },
-  {
     id: "genzql",
     title: "ZQL",
     tags: ["projects", "memes"],
@@ -52,7 +36,7 @@ Pretium morbi sodales blandit, eleifend aliquam aptent euismod nibh. Scelerisque
     description: "solve it once and for all",
   },
   {
-    id: "9",
+    id: "redgates",
     title: "On Red Gates",
     description: "... Coming soon!",
     date: new Date(),
