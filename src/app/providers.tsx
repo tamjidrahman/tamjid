ae53092367f4d388deab23dcfce3f428713ec948
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-10-09 07:38:56 -0400

    console log

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,11 +3,12 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
+console.log(process.env.NEXT_PUBLIC_POSTHOG_HOST);
+
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only",
-    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    person_profiles: "always",
   });
 }
 
