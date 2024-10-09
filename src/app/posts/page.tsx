3ed4099b4653b40da702240e732d4f7af05bd338
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-10-09 15:03:54 -0400

    remove suspense

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,9 +2,5 @@ import { Suspense } from "react";
 import PostPage from "./PostPage"; // Your component
 
 export default function PageWrapper() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <PostPage />
-    </Suspense>
-  );
+  return <PostPage />;
 }
